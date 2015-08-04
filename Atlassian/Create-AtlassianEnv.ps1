33a806e9d388acf98db90118e2075ef7cd5efe00
seward <Robert.Seward@Walgreens.com>
2015-08-03 22:37:07 -0500

    Massive updates and improvements.  Ready to test live now.

--- Atlassian/Create-AtlassianEnv.ps1
+++ Atlassian/Create-AtlassianEnv.ps1
@@ -105,40 +105,131 @@ Write-ColorOutput "Magenta" ">> RETURN-CODES: [$functionRc|$functionLEC|$functio
 
 }
 
+######################################################################################################
+# INITIALIZE
+#
+
+Set-PSDebug -trace 0 -strict
 $Error.Clear()
 
 Clear-Screen
 
 Set-StrictMode -Version Latest
-# Set-StrictMode -Off
 
+Set-PSDebug -trace 1 -strict
+# For Testing here
 Set-PSDebug -trace 0 -strict
-# Set-PSDebug -Off
+# Exit
 
-# Detailed Debugging:
-# $DebugPreference = 'Continue'
-# $VerbosePreference = 'Continue'
 #
-# Default Debugging:
-# $DebugPreference = 'SilentlyContinue'
-# $VerbosePreference = 'SilentlyContinue'
+# END INITIALIZE
+######################################################################################################
 
+
+######################################################################################################
+# INITIALIZE Variables
+#
 $ProjectPrefix = 'atlass'
 $DataCenterPrefix = 'wu'
 
-# Sign in to your Azure account
-$thisCommand = "Add-AzureAccount"
-Execute_Command 1 "$thisCommand"
-$thisRc=$?
-Write-ColorOutput "Magenta" "BOBFIX-RETURN(A-AA): [$thisRc|$ecRc]"
-
-# Initialize variables
+#-----------------------------------------------------------------------------------------------------
+# Initialize Azure variables
 $subscriptionName = 'Atlassian Subscription (NSEN)'
 $subscriptionPROSsit = 'PROS SIT'
 $vnetName = 'Atlassian-VNet'
 $location = 'West US'
 $userName = 'wagsadmin'
 $password = 'Welcome!234'
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize Server "Naming" variables
+$theseTiers = @("WEB","APP","DB")
+$thisEnv="P"
+$theseOSTypes = @("WINDOWS","LINUX")
+$theseOSs = @("W","L")
+$theseTypes = @("WB","AP","DB")
+$thisDataCenterLoc="AZ"
+$thisApplication="ATL"
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize "Image-Types" variables
+$azureStdVMSize = "Standard_DS3"
+$azureHighPerfVMSize = "Standard_DS12"
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize Nework and IP-Pools variables
+$newSubnets = @("WBA-ATL-WEB","WBA-ATL-APP","WBA-ATL-DB")
+$newWebIPAddrs = @("10.217.0.4","10.217.0.5","10.217.0.6","10.217.0.7","10.217.0.8","10.217.0.9","10.217.0.10")
+$newAppIPAddrs = @("10.217.0.68","10.217.0.69","10.217.0.70","10.217.0.71","10.217.0.72","10.217.0.73","10.217.0.74")
+$newDBIPAddrs = @("10.217.0.132","10.217.0.133","10.217.0.134","10.217.0.135","10.217.0.136","10.217.0.137","10.217.0.138")
+$newIPAddrs = $newWebIPAddrs, $newAppIPAddrs, $newDBIPAddrs
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize Storage Type name extensions
+$newStdStorageNames = @()
+$newStdStorageTypes = @('web1', 'app1', 'img1')
+$newHighPerfStorageNames = @()
+$newHighPerfStorageTypes = @('db1')
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize Cloud Services array
+$newCloudServices = @()
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize Container names
+$srcContainer = 'vhds'
+$destContainer = 'vhds'
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize Image names and family
+$imageWindowsNames = @('wagswin2012r2app1', 'wagswin2012r2app2', 'wagswin2012r2db1', 'wagswin2012r2as1')
+$imageLinuxNames = @('wagslinuxweb1', 'wagslinuxapp2', 'wagslinuxdb1', 'wagslinuxdb2')
+$imageFamily = 'Walgreens'
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize VMLists
+$VMList = @()
+$windowsVMList = @()
+$linuxVMList = @()
+
+#-----------------------------------------------------------------------------------------------------
+# Initialize Endpoints
+$sshEndpointName = 'SSH'
+$sshEndpointPort = '57101'
+#---
+$rdpEndpointName = 'RemoteDesktop'
+$rdpEndpointPort = '57201'
+#---
+$domainJoin = 'devwalgreenco.net'
+$domain = 'walgreenco'
+#---
+$ou = 'OU=Azure,OU=Servers,DC=devwalgreenco,DC=net'
+#
+Write-ColorOutput "Red" "BOBFIX-NEED-TO-FIX(???): [`$dns UNDEFINED]"
+$dns = $null
+#
+######################################################################################################
+
+
+######################################################################################################
+# DEPRECATED Information
+#
+#-----------------------------------------------------------------------------------------------------
+# $newCloudServices = @('pccperfwuweb1', 'pccperfwuapp1', 'pccperfwudb1')
+#-----------------------------------------------------------------------------------------------------
+#
+######################################################################################################
+
+
+######################################################################################################
+# Begin Azure work
+######################################################################################################
+# Sign in to your Azure account
+$thisCommand = "Add-AzureAccount"
+Execute_Command 1 "$thisCommand"
+$thisRc=$?
+Write-ColorOutput "Magenta" "BOBFIX-RETURN(A-AA): [$thisRc|$ecRc]"
+
 
 # Set the current subscription
 $thisCommand = "Select-AzureSubscription -SubscriptionName `"$subscriptionName`""
@@ -147,10 +238,6 @@ $thisRc=$?
 Write-ColorOutput "Magenta" "BOBFIX-RETURN(S-AS): [$thisRc|$ecRc]"
 
 # Create the Standard storage accounts
-$newStdStorageNames = @()
-$newCloudServices = @()
-$newStdStorageTypes = @('web1', 'app1', 'img1')
-
 foreach ($newStorageType in $newStdStorageTypes)
 {
 
@@ -175,11 +262,9 @@ Write-ColorOutput "Red" "BOBFIX-ERROR(G-ASA): [$Global:ecErrorVar]"
     }
 
 }
+$destStorageAccounts = $newStdStorageNames
 
-# Create the Standard storage accounts
-$newHighPerfStorageNames = @()
-$newHighPerfStorageTypes = @('db1')
-
+# Create the High-Performance storage accounts
 foreach ($newStorageType in $newHighPerfStorageTypes)
 {
 
@@ -204,6 +289,7 @@ Write-ColorOutput "Red" "BOBFIX-ERROR(G-ASA): [$Global:ecErrorVar]"
     }
 
 }
+$destStorageAccounts += $newHighPerfStorageNames
 
 # Set the current storage account (not required but a good practice)
 $thisCommand = "Set-AzureSubscription -SubscriptionName `"$subscriptionName`" -CurrentStorageAccountName `"$($newStdStorageNames[0])`""
@@ -212,29 +298,21 @@ Execute_Command 0 "$thisCommand"
 # Copy the Windows VM image VHDs to the storage accounts
 $srcStorageAccount = 'ppsssitwuimg1'
 
-# BOBFIX STARTING (DELETE TILL COMPLETE WHEN FIXED By Azure)
-# Write-ColorOutput "Red" "BOBFIX: OVERRIDE Storage device[ppsssitwuimg1]"
-# $srcStorageAccount = 'atlasswudb1'
-# BOBFIX COMPLETE (DELETE Back to STARTING WHEN FIXED By Azure)
-
-# BOBFIX STARTING (DELETE TILL COMPLETE WHEN FIXED By Azure)
-Write-ColorOutput "Red" "BOBFIX: OVERRIDE Select-AzureSubscription [Atlassian Subscription (NSEN)]"
+# NOTE: STARTING (Need to change selected subscription to pull the key in the next step)
 $thisCommand = "Select-AzureSubscription -SubscriptionName `"$subscriptionPROSsit`""
 Execute_Command 0 "$thisCommand"
-# BOBFIX COMPLETE (DELETE Back to STARTING WHEN FIXED By Azure)
+# NOTE: COMPLETE
 
 $thisCommand = "Get-AzureStorageKey -StorageAccountName $srcStorageAccount"
 Write-ColorOutput "Green" ">> EXECUTE: `$srcStorageKey = (Invoke-Expression $thisCommand).Primary"
 $srcStorageKey = (Invoke-Expression $thisCommand).Primary
 Write-ColorOutput "Cyan" "BOBFIX-OUTPUT: $srcStorageKey"
 
-# BOBFIX STARTING (DELETE TILL COMPLETE WHEN FIXED By Azure)
-Write-ColorOutput "Red" "BOBFIX: RESET-OVERRIDE Select-AzureSubscription [Atlassian Subscription (NSEN)]"
+# NOTE STARTING:  (Need to change selected subscription Back to this subscription after retrieving key)
 $thisCommand = "Select-AzureSubscription -SubscriptionName `"$subscriptionName`""
 Execute_Command 0 "$thisCommand"
-# BOBFIX COMPLETE (DELETE Back to STARTING WHEN FIXED By Azure)
+# NOTE: COMPLETE
 
-Set-PSDebug -trace 0 -strict
 
 #$srcContext = New-AzureStorageContext –StorageAccountName $srcStorageAccount -StorageAccountKey $srcStorageKey
 $thisCommand = "New-AzureStorageContext –StorageAccountName $srcStorageAccount -StorageAccountKey $srcStorageKey"
@@ -242,13 +320,9 @@ Write-ColorOutput "Green" ">> EXECUTE: `$srcContext = Invoke-Expression $thisCom
 $srcContext = Invoke-Expression $thisCommand
 Write-ColorOutput "Cyan" "BOBFIX-OUTPUT: $srcContext"
 
-$destStorageAccounts = $newStdStorageNames
-$destStorageAccounts += $newHighPerfStorageNames
 
 Clear-Ten
 
-$srcContainer = 'vhds'
-$destContainer = 'vhds'
 
 foreach ($destStorageAccount in $destStorageAccounts)
 {
@@ -267,6 +341,7 @@ Write-ColorOutput "Cyan" "BOBFIX-OUTPUT(N-ASCx): $srcContext"
 
 # Write-Output "	BOBFIX >>>> " `
 #    New-AzureStorageContainer -Name vhds -Context $destContext
+Write-ColorOutput "Red" "BOBFIX: Do a test with Get-AzureStorageContainer"
     $thisCommand = "New-AzureStorageContainer -Name vhds -Context $destContext"
     Execute_Command 1 "$thisCommand"
     $thisRc = $?
@@ -313,14 +388,12 @@ Write-Output "	BOBFIX >>>> `
 
 }
 
-# Create the Windows VM Image objects
-$imageWindowsNames = @('wagswin2012r2app1', 'wagswin2012r2app2', 'wagswin2012r2db1', 'wagswin2012r2as1')
-$imageLinuxNames = @('wagslinuxweb1', 'wagslinuxapp2', 'wagslinuxdb1', 'wagslinuxdb2')
-$imageFamily = 'Walgreens'
-$count = 0
 
 Clear-Ten
 
+
+# Create the VM Image objects
+$count = 0
 foreach ($destStorageAccount in $destStorageAccounts)
 {
 #    $imageDisk = 'https://{0}.blob.core.windows.net/vhds/2012R2Image.vhd' -f $destStorageAccount
@@ -355,9 +428,10 @@ Write-ColorOutput "Magenta" "BOBFIX-RETURN(A-AVMI): [$thisRc|$ecRc]"
     $count++
 }
 
+
 Clear-Ten
 
-# $newCloudServices = @('pccperfwuweb1', 'pccperfwuapp1', 'pccperfwudb1')
+
 Write-ColorOutput "Cyan" "BOBFIX-(Cloud-Services): [$newCloudServices]"
 
 foreach ($newCloudService in $newCloudServices)
@@ -377,67 +451,137 @@ Write-ColorOutput "Magenta" "BOBFIX-RETURN(A-AVMI): [$thisRc|$ecRc]"
 
 }
 
-# Create Web VM #1
-$serviceName = 'pccperfwuweb1'
-$vmName = 'ULWB-AZPCC01'
-$imageName = 'wagslinuxweb1'
-$vmSize = 'Standard_D11'
-$avSetName = 'pccperfwuwebavset'
-$storageAccount = 'pccperfwuweb1'
-Write-Output "	BOBFIX >>>> " `
-$osDisk = 'https://{0}.blob.core.windows.net/vhds/{1}-os-1.vhd' -f $storageAccount, $vmName
-Write-Output "	BOBFIX >>>> " `
-$dataDisk1 = 'https://{0}.blob.core.windows.net/vhds/{1}-data-1.vhd' -f $storageAccount, $vmName
-$sshEndpointName = 'SSH'
-$sshEndpointPort = '57101'
-$subnetName = 'WEB'
-$staticIPAddress = '172.17.38.4'
+
+# Set-PSDebug -trace 0 -strict
+for ($typeCount=0;$typeCount -lt 3;$typeCount++)
+{
+    $thisTier = $theseTiers[${typeCount}]
+    $thisType = $theseTypes[${typeCount}]
+    $entryMax = 1
+    if ($typeCount -eq 0 -or $typeCount -eq 2) { $entryMax = 1 }
+    else {
+	if ($typeCount -eq 1) { $entryMax = 4 }
+    }
+
+    for ($entryCount=0;$entryCount -lt $entryMax;$entryCount++)
+    {
+	$newAVSet = $newCloudServices[${typeCount}] -creplace '[0-9]*$',''
+	$newAVSet = "${newAVSet}avset"
+	$entryCountPrint = $($entryCount+1).ToString("00")
+	if ($typeCount -eq 1 -and $entryCount -eq 0) {
+	    $thisOS = $theseOSs[0]
+	    $thisOSType = $theseOSTypes[0]
+	    $thisImageName = $imageWindowsNames[${typeCount}]
+	    $thisEndpointName = $rdpEndpointName
+	    $thisEndpointPort = $rdpEndpointPort
+	}
+	else {
+	    $thisOS = $theseOSs[1]
+	    $thisOSType = $theseOSTypes[1]
+	    $thisImageName = $imageLinuxNames[${typeCount}]
+	    $thisEndpointName = $sshEndpointName
+	    $thisEndpointPort = $sshEndpointPort
+	}
+	if ($typeCount -eq 2) {
+	    $thisImageSize = $azureHighPerfVMSize
+	}
+	else {
+	    $thisImageSize = $azureStdVMSize
+	}
+	$thisVMName = "$thisEnv$thisOS$thisType-$thisDataCenterLoc$thisApplication$entryCountPrint"
+
+	$VMList += @(,($thisVMName, `
+			$thisTier, `
+			$newCloudServices[${typeCount}], `
+			$thisOSType, `
+			$thisImageName, `
+			$thisImageSize, `
+			$newAVSet, `
+			$newStdStorageNames[${typeCount}], `
+			$newSubnets[${typeCount}], `
+			$newIPAddrs[${typeCount}][${entryCount}], `
+			$thisEndpointName, $thisEndpointPort `
+		))
+    }
+}
+
+
+Set-PSDebug -trace 1 -strict
+$VMList[0]
+$VMList[1]
+$VMList[2]
+$VMList[3]
+$VMList[4]
+$VMList[5]
+
+$VMList.count
+
+Set-PSDebug -trace 1 -strict
+for($entryCount = 0; $entryCount -lt $VMList.count; $entryCount++)
+{
+    $vmName = $VMList[${entryCount}][0]
+    $tierType = $VMList[${entryCount}][1]
+    $serviceName = $VMList[${entryCount}][2]
+    $osType = $VMList[${entryCount}][3]
+    $imageName = $VMList[${entryCount}][4]
+    $vmSize = $VMList[${entryCount}][5]
+    $avSetName = $VMList[${entryCount}][6]
+    $storageAccount = $VMList[${entryCount}][7]
+    $subnetName = $VMList[${entryCount}][8]
+    $staticIPAddress = $VMList[${entryCount}][9]
+    $thisEndpointName = $VMList[${entryCount}][10]
+    $thisEndpointPort = $VMList[${entryCount}][11]
+    $osDisk = 'https://{0}.blob.core.windows.net/vhds/{1}-os-1.vhd' -f $storageAccount, $vmName
+    $dataDisk1 = 'https://{0}.blob.core.windows.net/vhds/{1}-data-1.vhd' -f $storageAccount, $vmName
+    $dataDisk2 = 'https://{0}.blob.core.windows.net/vhds/{1}-data-2.vhd' -f $storageAccount, $vmName
+
+    if ($osType -eq "LINUX")
+    {
+Write-ColorOutput "Red" "BOBFIX-NEED-TO-FIX(???): [How many disks???]"
+Write-ColorOutput "Yellow" ">> BOBFIX TESTING >>>> " `
 
 Set-PSDebug -trace 0 -strict
 Exit
+	New-AzureVMConfig -Name $vmName -InstanceSize $vmSize -ImageName $imageName -AvailabilitySetName $avSetName -MediaLocation $osDisk | `
+		Add-AzureProvisioningConfig -Linux -LinuxUser $userName -Password $password | `
+		Set-AzureEndpoint -Name $thisEndpointName -PublicPort $thisEndpointPort | `
+		Set-AzureSubnet -SubnetNames $subnetName | `
+		Set-AzureStaticVNetIP -IPAddress $staticIPAddress | `
+		Add-AzureDataDisk -CreateNew -DiskSizeInGB 1000 -DiskLabel 'Data 1' -LUN 1 -MediaLocation $dataDisk1 | `
+		Add-AzureDataDisk -CreateNew -DiskSizeInGB 1000 -DiskLabel 'Data 2' -LUN 2 -MediaLocation $dataDisk2 | `
+		New-AzureVM -ServiceName $serviceName -VNetName $vnetName
 
-Write-Output "	BOBFIX >>>> " `
-New-AzureVMConfig -Name $vmName -InstanceSize $vmSize -ImageName $imageName -AvailabilitySetName $avSetName -MediaLocation $osDisk | `
-    Add-AzureProvisioningConfig -Linux -LinuxUser $userName -Password $password | `
-    Set-AzureEndpoint -Name $sshEndpointName -PublicPort $sshEndpointPort | `
-    Set-AzureSubnet -SubnetNames $subnetName | `
-    Set-AzureStaticVNetIP -IPAddress $staticIPAddress | `
-    Add-AzureDataDisk -CreateNew -DiskSizeInGB 1000 -DiskLabel 'Data 1' -LUN 1 -MediaLocation $dataDisk1 | `
-    New-AzureVM -ServiceName $serviceName -VNetName $vnetName
+    }
+    else
+    {
+	if ($osType -eq "WINDOWS")
+	{
+Write-ColorOutput "Red" "BOBFIX-NEED-TO-FIX(???): [How many disks???]"
+Write-ColorOutput "Red" "BOBFIX-NEED-TO-FIX(???): [`$dns UNDEFINED]"
+Write-ColorOutput "Yellow" ">> BOBFIX TESTING >>>> " `
+	New-AzureVMConfig -Name $vmName -InstanceSize $vmSize -ImageName $imageName -AvailabilitySetName $avSetName -MediaLocation $osDisk | `
+
+Set-PSDebug -trace 0 -strict
+Exit
+		Add-AzureProvisioningConfig -WindowsDomain -JoinDomain $domainJoin -Domain $domain -AdminUsername $userName -Password $password -MachineObjectOU $ou -NoWinRMEndpoint | `
+		Set-AzureEndpoint -Name $rdpEndpointName -PublicPort $rdpEndpointPort | `
+		Set-AzureSubnet -SubnetNames $subnetName | `
+		Set-AzureStaticVNetIP -IPAddress $staticIPAddress | `
+		Add-AzureDataDisk -CreateNew -DiskSizeInGB 1000 -DiskLabel 'Data 1' -LUN 1 -MediaLocation $dataDisk1 | `
+		Add-AzureDataDisk -CreateNew -DiskSizeInGB 1000 -DiskLabel 'Data 2' -LUN 2 -MediaLocation $dataDisk2 | `
+		New-AzureVM -ServiceName $serviceName -VNetName $vnetName -DnsSettings $dns
+
+	}
+    }
+
+Set-PSDebug -trace 0 -strict
+Exit
+}
+
 
 # Create App VM #1
-$serviceName = 'ppssuatptwuapp'
-$vmName = 'ppssuatptwuapp1'
-$imageName = 'wagswin2012r2app1'
-$vmSize = 'Standard_DS14'
-$avSetName = 'ppssuatptwuappavset'
-$storageAccount = 'ppssuatptwuapp1'
-Write-Output "	BOBFIX >>>> " `
-$osDisk = 'https://{0}.blob.core.windows.net/vhds/{1}-os-1.vhd' -f $storageAccount, $vmName
-Write-Output "	BOBFIX >>>> " `
-$dataDisk1 = 'https://{0}.blob.core.windows.net/vhds/{1}-data-1.vhd' -f $storageAccount, $vmName
-Write-Output "	BOBFIX >>>> " `
-$dataDisk2 = 'https://{0}.blob.core.windows.net/vhds/{1}-data-2.vhd' -f $storageAccount, $vmName
-$rdpEndpointName = 'RemoteDesktop'
-$rdpEndpointPort = '57201'
-$subnetName = 'App'
-$staticIPAddress = '172.17.34.69'
-$domainJoin = 'devwalgreenco.net'
-$domain = 'walgreenco'
-$ou = 'OU=Azure,OU=Servers,DC=devwalgreenco,DC=net'
-
 Set-PSDebug -trace 0 -strict
 Exit
-
-Write-Output "	BOBFIX >>>> " `
-New-AzureVMConfig -Name $vmName -InstanceSize $vmSize -ImageName $imageName -AvailabilitySetName $avSetName -MediaLocation $osDisk | `
-	Add-AzureProvisioningConfig -WindowsDomain -JoinDomain $domainJoin -Domain $domain -AdminUsername $userName -Password $password -MachineObjectOU $ou -NoWinRMEndpoint | `	
-    Set-AzureEndpoint -Name $rdpEndpointName -PublicPort $rdpEndpointPort | `
-    Set-AzureSubnet -SubnetNames $subnetName | `
-    Set-AzureStaticVNetIP -IPAddress $staticIPAddress | `
-    Add-AzureDataDisk -CreateNew -DiskSizeInGB 1000 -DiskLabel 'Data 1' -LUN 1 -MediaLocation $dataDisk1 | `
-    Add-AzureDataDisk -CreateNew -DiskSizeInGB 1000 -DiskLabel 'Data 2' -LUN 2 -MediaLocation $dataDisk2 | `
-    New-AzureVM -ServiceName $serviceName -VNetName $vnetName -DnsSettings $dns
 
 # Associate the Reserved VIPs with the cloud services
 Write-Output "	BOBFIX >>>> " `
@@ -449,4 +593,4 @@ Set-AzureReservedIPAssociation -ReservedIPName 'pccperfwudb1vip' -ServiceName 'p
 
 
 Set-PSDebug -trace 0 -strict
-ExitzureStorageAccount -StorageAccountName "atlasswuweb1"
+Exit
