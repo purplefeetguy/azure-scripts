ae519498683345c12c8a229a7a27fe2a46210736
seward <Robert.Seward@Walgreens.com>
2015-08-15 13:38:24 -0500

    Many updates to handle different situations

--- Atlassian/Create-DB-LUNs.sh
+++ Atlassian/Create-DB-LUNs.sh
@@ -1,61 +1,84 @@
 #!/bin/bash
-printf "\n\n\n\n\n\n\n\n\n\n"
+printf "\n\n\n\n\n\n\n"
 
-CREATE_LUNS="FALSE"
+CREATE_LUNS="TRUE"
 CREATE_MDADM="FALSE"
-CREATE_FS="TRUE"
+CREATE_FS="FALSE"
 
-MDADM_NUMS="1 2 3 4"
+DISK_CHARS="c d e"
+
+MDADM_MAX=1
+MDADM_SIZE[${MDADM_MAX}]="+200G"
+MDADM_MAX=$((MDADM_MAX+1))
+MDADM_SIZE[${MDADM_MAX}]="+200G"
+MDADM_MAX=$((MDADM_MAX+1))
+MDADM_SIZE[${MDADM_MAX}]="+40G"
+MDADM_MAX=$((MDADM_MAX+1))
+MDADM_SIZE[${MDADM_MAX}]="+72G"
+MDADM_SIZE[${MDADM_MAX}]=
+
+set -x
+# MDADM_MAX=1
+# DISK_CHARS="c"
+set +x
+
 MDADM_PREFIX="md"
 MDADM_START_NUM="10"
 MDADM_START="${MDADM_PREFIX}${MDADM_START_NUM}"
 
 if [ "${CREATE_LUNS}" = "TRUE" ]; then
-    for disk in c d e;do
-	set -x
-	(
-	for num in 1 2;do
-	    size="+200G"
-	    printf "n\np\n${num}\n\n${size}\nt\n${num}\nfd\n"
-	done; \
-	for num in 3;do
-	    size="+40G"
-	    printf "n\np\n${num}\n\n${size}\nt\n${num}\nfd\n"
-	done; \
-	for num in 4;do
-	    size="+72G"
-	    size=
-	    printf "n\np\n${num}\n\n${size}\nt\n${num}\nfd\n"
-	done; \
-	printf "w\n" \
-	) | fdisk /dev/sd${disk}
-	set +x
-	printf "\n\n\n\n\n\n\n\n\n\n"
+    for disk in ${DISK_CHARS};do
+	mdadmNum=1
+	INPUT_STRING=
+	while [ "${mdadmNum}" -le "${MDADM_MAX}" ];do
+	    thisPartition=
+	    if [ "${mdadmNum}" -gt "1" ]; then
+		thisPartition="${mdadmNum}\n"
+	    fi
+	    thisSize="${MDADM_SIZE[${mdadmNum}]}"
+	    thisInput="n\np\n${mdadmNum}\n\n${thisSize}\nt\n${thisPartition}fd\n"
+	    INPUT_STRING="${INPUT_STRING}${thisInput}"
+	    mdadmNum=$((mdadmNum+1))
+	done
+	printf "PROCESSING:\n"
+	printf "${INPUT_STRING}w\n"
+	printf "\--------------------------------------------\n"
+	printf "${INPUT_STRING}w\n" \
+		| fdisk /dev/sd${disk}
+	printf "\n\n\n"
     done
 fi
 
-for disk in c d e;do
-    printf "\n\n\n\n\n\n\n\n\n\n"
+for disk in ${DISK_CHARS};do
+    printf "\n\n\n\n\n"
     echo "p\n" | fdisk /dev/sd${disk}
 done
 
 if [ "${CREATE_MDADM}" = "TRUE" ]; then
-    printf "\n\n\n\n\n\n\n\n\n\n"
-    for mdadmNum in ${MDADM_NUMS};do
+    printf "\n\n\n\n\n"
+    mdadmNum=1
+    while [ "${mdadmNum}" -le "${MDADM_MAX}" ];do
 	set -x
 	mdadm --create /dev/${MDADM_START}${mdadmNum} --level 0 --raid-devices 3 \
 		/dev/sdc${mdadmNum} \
 		/dev/sdd${mdadmNum} \
 		/dev/sde${mdadmNum}
 	set +x
+	mdadmNum=$((mdadmNum+1))
     done
+    ls -al /dev/${MDADM_PREFIX}*
 fi
 
 if [ "${CREATE_FS}" = "TRUE" ]; then
-    printf "\n\n\n\n\n\n\n\n\n\n"
-    for mdadmNum in ${MDADM_NUMS};do
+    printf "\n\n\n\n\n"
+    mdadmNum=1
+    while [ "${mdadmNum}" -le "${MDADM_MAX}" ];do
+#    for mdadmNum in 126 124 127 125
 	set -x
-	mkfs.ext4 -E lazy_journal_init=1,lazy_itable_init=1 /dev/${MDADM_START}${mdadmNum}
+#	mkfs.ext4 -E lazy_journal_init=1,lazy_itable_init=1 /dev/${MDADM_START}${mdadmNum}
+#	mkfs.ext4 -E lazy_itable_init=1 /dev/${MDADM_START}${mdadmNum}
+	mkfs.ext4 -E lazy_itable_init=1 /dev/${MDADM_PREFIX}${mdadmNum}
 	set +x
+	mdadmNum=$((mdadmNum+1))
     done
 fi
