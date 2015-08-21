c2a7341fa83877d12bba5a626bd5a938f7b1abf5
seward <Robert.Seward@Walgreens.com>
2015-08-21 13:04:26 -0500

    Initial working copy of NSG-Atlassian.ps1

--- Atlassian/NSG-Atlassian.ps1
+++ Atlassian/NSG-Atlassian.ps1
@@ -14,6 +14,12 @@
     1.0  7/27/2015  Ed Mondek  Initial commit
 #>
 
+Set-PSDebug -trace 0 -strict
+
+. "../../PS_Funcs/PS_Funcs_Std.ps1"
+Clear-Ten
+$LASTEXITCODE = $false
+
 # Sign in to your Azure account
 #Add-AzureAccount
 
@@ -29,8 +35,53 @@ Select-AzureSubscription -SubscriptionName $subscriptionName
 $nsgName = 'ATL-NSG'
 $subnetName = 'WBA-ATL-WEB'
 
-# Create the NSG for the Web subnet if it does not already exist
-$atlNSG = New-AzureNetworkSecurityGroup -Name $nsgName -Location $location -Label 'Network Security Group for all subnets in West US'
+
+$PRIORITY_START = 1000
+$SOURCE_ADDRESSES = @("83.103.45.48/28")
+$sourceAddress = $SOURCE_ADDRESSES[0]
+
+
+    $thisCommand = "Get-AzureNetworkSecurityGroup -Name $nsgName"
+    $testCommand = "Get-AzureNetworkSecurityGroup -Name $nsgName -Detailed"
+    Execute_Command 0 "$testCommand"; $thisRc = $?
+    if ($Global:ecOutput -eq $null) { $Global:ecRc = $false }
+    Write-ColorOutput "Magenta" "BOBFIX-RETURN[G-ANSG-Test]: [$thisRc|$Global:ecRc]"
+
+    if ($Global:ecRc -eq $false) {
+	Write-ColorOutput "Magenta" "BOBFIX-NOT_CREATED[G-ANSG]: [$Global:ecVariableError]"
+
+	# Create the NSG for the Web subnet if it does not already exist
+	$thisCommand = "New-AzureNetworkSecurityGroup -Name $nsgName -Location $location -Label 'Network Security Group for all subnets in West US'"
+	Write-ColorOutput "Green" ">> EXECUTE: `$atlNSG = Invoke-Expression $thisCommand"
+	$thisRc = $Global:ecRc
+# BOBFIX	$atlNSG = Invoke-Expression $thisCommand; $thisRc = $?
+	$Global:ecRc = $thisRc
+	Write-ColorOutput "Magenta" "BOBFIX-RETURN[N-ANSG]: [$thisRc|$Global:ecRc]"
+	Write-ColorOutput-SingleQ "Cyan" 'BOBFIX-OUTPUT[N-ANSG-atlNSG]: $atlNSG'
+
+    } else {
+	Write-ColorOutput-SingleQ "Cyan" 'BOBFIX-OUTPUT[G-ANSG-Test]: $Global:ecOutput'
+
+	Write-ColorOutput "Green" ">> EXECUTE: `$atlNSG = Invoke-Expression $thisCommand"
+	$thisRc = $Global:ecRc
+	$atlNSG = Invoke-Expression $thisCommand; $thisRc = $?
+	$Global:ecRc = $thisRc
+	Write-ColorOutput "Magenta" "BOBFIX-RETURN[G-ANSG-atlNSG]: [$thisRc|$Global:ecRc]"
+	Write-ColorOutput-SingleQ "Cyan" 'BOBFIX-OUTPUT[G-ANSG-atlNSG]: $atlNSG'
+    }
+
+    if ($Global:ecRc -eq $true) {
+	$ruleCount = 9
+	$priorityCount = $PRIORITY_START + $ruleCount
+
+	$thisCommand = "Set-AzureNetworkSecurityRule -NetworkSecurityGroup "+'$atlNSG'+" -Name 'Internet_Inbound_Allow_$ruleCount' -Type Inbound -Priority $priorityCount -Action Allow -SourceAddressPrefix '$sourceAddress' -SourcePortRange '*' -DestinationAddressPrefix '*' -DestinationPortRange '*' -Protocol 'TCP'"
+	Execute_Command 1 "$thisCommand"; $thisRc = $?
+
+    }
+
+
+
+Set-PSDebug -trace 0 -strict;Exit 1
 
 # Get info for the NSG assigned to the Web subnet
 <#
@@ -60,3 +111,5 @@ $atlNSG | Set-AzureNetworkSecurityRule -Name 'Internet_Inbound_Allow_3' -Type In
 $atlNSG | Set-AzureNetworkSecurityGroupToSubnet -VirtualNetworkName $vnetName -SubnetName WBA-ATL-WEB
 $atlNSG | Set-AzureNetworkSecurityGroupToSubnet -VirtualNetworkName $vnetName -SubnetName WBA-ATL-APP
 $atlNSG | Set-AzureNetworkSecurityGroupToSubnet -VirtualNetworkName $vnetName -SubnetName WBA-ATL-DB
+
+Exit 1
