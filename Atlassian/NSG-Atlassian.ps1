25bcd80f59644e0ac4f46f4f5b43cb42e507764d
seward <Robert.Seward@Walgreens.com>
2015-08-22 19:09:48 -0500

    Enhancements and the rest of the Azure defined subnets

--- Atlassian/NSG-Atlassian.ps1
+++ Atlassian/NSG-Atlassian.ps1
@@ -31,13 +31,22 @@ $Global:LOCATION = 'West US'
 
 ### Configure the NSG for the Web subnet ###
 $Global:NSG_NAME = 'ATL-NSG'
-$Global:SUBNET_NAMES = @('WBA-ATL-WEB','WBA-ATL-APP','WBA-ATL-DB')
-
-
+#$Global:SUBNET_NAMES = @('WBA-ATL-WEB','WBA-ATL-APP','WBA-ATL-DB')
+$Global:SUBNET_NAMES = @()
+$Global:SUBNET_NAMES += 'WBA-ATL-WEB'
+$Global:SUBNET_NAMES += 'WBA-ATL-APP'
+$Global:SUBNET_NAMES += 'WBA-ATL-DB'
+$Global:SUBNET_NAMES += 'WBA-ATL-INF'
+$Global:SUBNET_NAMES += 'WBA-ATL-RES-1'
+$Global:SUBNET_NAMES += 'WBA-ATL-VIP'
+$Global:SUBNET_NAMES += 'WBA-ATL-RES-2'
+$Global:SUBNET_NAMES += 'WBA-ATL-MGT-WEB'
+$Global:SUBNET_NAMES += 'WBA-ATL-MGT-APP'
+$Global:SUBNET_NAMES += 'WBA-ATL-MGT-DB'
+$Global:SUBNET_NAMES += 'WBA-ATL-MGT-INF'
+$Global:SUBNET_NAMES += 'WBA-ATL-MGT-RES2'
+$Global:SUBNET_NAMES += 'Gateway'
 Write-ColorOutput "Red" "BOBFIX-CHANGE:  Need to handle all subnets"
-Set-PSDebug -trace 1 -strict
-$Global:subnetName = 'WBA-ATL-WEB'
-Set-PSDebug -trace 0 -strict
 
 
 ################################################################################
@@ -105,9 +114,21 @@ Write-ColorOutput "$thisColor" "BOBFIX:  Creation of Network Security Group RULE
 
 
 ################################################################################
+# Setup Perform or not perform Creation of NSG Subnet
+#
+$Global:PERFORM_CREATE_SUBNET = $true
+if ($Global:PERFORM_CREATE_SUBNET -eq $true) { $Global:performCreateSubnet = 0; $thisColor = "Green" } else { $Global:performCreateSubnet = 1; $thisColor = "Red" }
+Write-ColorOutput "$thisColor" "BOBFIX:  Creation of Network Security Group RULEs [GREEN:ENABLED / RED:DISABLED]"
+# $Global:PERFORM_CREATE_SUBNET; $Global:performCreateSubnet
+#
+################################################################################
+
+
+################################################################################
 # Setup Base Get-NSG command
 #
 $Global:GET_NSG_BASE = "Get-AzureNetworkSecurityGroup -Name $NSG_NAME"
+$Global:GET_NSG_SUBNET_BASE = "Get-AzureNetworkSecurityGroupForSubnet -VirtualNetworkName '$VNET_NAME' -SubnetName"
 #
 ################################################################################
 
@@ -163,7 +184,6 @@ function Get_NSG_Base()
 #
 function Create_NSG()
 {
-Set-PSDebug -trace 1 -strict
     # Create the NSG for the Web subnet if it does not already exist
     $createCommand = "New-AzureNetworkSecurityGroup -Name $NSG_NAME -Location $LOCATION -Label 'Network Security Group for all subnets in West US'"
     if ($PERFORM_CREATE_NSG -eq $true) {
@@ -179,6 +199,12 @@ Set-PSDebug -trace 1 -strict
 	Write-ColorOutput "Yellow" ">> TESTING: `$Global:currentNSG = Invoke-Expression $thisCommand"
     }
 # Set-PSDebug -trace 0 -strict
+#-------------------------------------------------------------------------------
+# To Remove the NSG
+<#
+Remove-AzureNetworkSecurityGroup -Name $NSG_NAME
+#>
+#-------------------------------------------------------------------------------
 }
 #
 ################################################################################
@@ -215,7 +241,6 @@ function Setup_NSG()
 function Create_NSG_Rule($cnrName)
 {
     $cnrPortNumber = $INTERNET_PORT_NUMBERS.$Global:sourcePortType
-$cnrPortNumber
 
     $thisCommand = "Set-AzureNetworkSecurityRule -NetworkSecurityGroup "+'$Global:currentNSG'+" -Name '$Global:thisNSGRuleName' -Type Inbound -Priority $Global:thisPriorityNumber -Action Allow -SourceAddressPrefix '$sourceAddress' -SourcePortRange '*' -DestinationAddressPrefix '*' -DestinationPortRange '$cnrPortNumber' -Protocol 'TCP'"
     Execute_Command $Global:performCreateRule "$thisCommand"; $thisRc = $?
@@ -239,14 +264,14 @@ $cnrPortNumber
 ################################################################################
 # Setup_NSG_Rules
 #
-function Setup_NSG_Rules($snrName)
+function Setup_NSG_Rules()
 {
 
     foreach($Global:sourcePortType in $Global:INTERNET_PORT_NAMES) {
 	if($Global:sourcePortType -ne "STAR") { Write-ColorOutput "Red" "BOBFIX-CHANGE:  Need to handle all required ports: Skipping[$Global:sourcePortType]"; continue }
 	$snrRuleCount = 1
 	foreach($sourceAddress in $SOURCE_ADDRESSES) {
-	    $Global:thisPriorityNumber = $PRIORITY_START.STAR + $snrRuleCount
+	    $Global:thisPriorityNumber = $PRIORITY_START.$Global:sourcePortType + $snrRuleCount
 	    $snrPrintRuleCount = $snrRuleCount.TOString("00")
 	    $Global:thisNSGRuleName = "$Global:INTERNET_PREFIX-$Global:sourcePortType-$snrPrintRuleCount"
 
@@ -255,17 +280,98 @@ function Setup_NSG_Rules($snrName)
 #	    Write-ColorOutput "Magenta" "BOBFIX-RETURN[N-ANSG]: [$thisRc|$Global:ecRc]"
 	    if ($thisRc -eq $false -or $testEntry -eq $null ) {
 		Create_NSG_Rule "$cnrName"
-if ($Global:PERFORM_CREATE_RULE) { Set-PSDebug -trace 0 -strict;Exit 1 }
+# if ($Global:PERFORM_CREATE_RULE) { Set-PSDebug -trace 0 -strict;Exit 1 }
 	    }
 #	    else { Write-ColorOutput-SingleQ "Yellow" 'BOBFIX-OUTPUT[NSG-Rule-Exists]:  $testEntry' }
 
 	    $snrRuleCount++
 	}
-   }
+    }
 
 }
 #
 ################################################################################
+
+
+################################################################################
+# Get_NSG_Subnet_Detailed - Get Network Security Group Subnet Detailed
+#
+function Get_NSG_Subnet_Detailed()
+{
+    $detailedCommand = "$Global:baseSubnetCommand -Detailed"
+
+    Execute_Command 0 "$detailedCommand"; $thisRc = $?
+    if ($Global:ecOutput -eq $null) { $Global:ecRc = $false }
+    Write-ColorOutput "Magenta" "BOBFIX-RETURN[G-ANSGS-Detailed]: [$thisRc|$Global:ecRc]"
+    if ($Global:ecRc -eq $true) {
+	$Global:NSG_SUBNET_DETAILED = $Global:ecOutput
+	Write-ColorOutput-SingleQ "Cyan" 'BOBFIX-OUTPUT[G-ANSGS-Detailed]: $Global:NSG_SUBNET_DETAILED'
+    }
+    else {
+	Write-ColorOutput "Magenta" "BOBFIX-NOT_CREATED[G-ANSGS-Detailed]: [$Global:ecVariableError]"
+    }
+
+}
+#
+################################################################################
+
+
+################################################################################
+# Create_NSG_Subnet
+#
+function Create_NSG_Subnet()
+{
+    $thisCommand = "Set-AzureNetworkSecurityGroupToSubnet -Name $Global:NSG_NAME -VirtualNetworkName '$Global:VNET_NAME' -SubnetName '$Global:subnetName'"
+    Execute_Command $Global:performCreateSubnet "$thisCommand"; $thisRc = $?
+    Write-ColorOutput "Magenta" "BOBFIX-RETURN[N-ANSR]: [$thisRc|$Global:ecRc]"
+    if ($PERFORM_CREATE_SUBNET ) {
+	if ($thisRc -eq $false -or $Global:ecRc -eq $false -or $Global:NSG_SUBNET_DETAILED -eq $null ) {
+	    Write-ColorOutput "Red" "BOBFIX-NOT_CREATED[S-ANSGTS] FAILED"
+	    Write-ColorOutput "Red" "BOBFIX-NOT_CREATED[S-ANSGTS]: [$Global:ecVariableError]"
+	}
+	else {
+	    Write-ColorOutput-SingleQ "Cyan" 'BOBFIX-OUTPUT[S-ANSGTS-Detailed]: $Global:NSG_SUBNET_DETAILED'
+	}
+    }
+
+#-------------------------------------------------------------------------------
+# To Remove the NSG assigned to a subnet
+<#
+Remove-AzureNetworkSecurityGroupFromSubnet -VirtualNetworkName '$Global:VNET_NAME' -SubnetName '$Global:subnetName' -Name $Global:NSG_NAME
+#>
+#-------------------------------------------------------------------------------
+}
+#
+################################################################################
+
+
+################################################################################
+# Setup_NSG_Subnets
+#
+function Setup_NSG_Subnets()
+{
+
+    foreach($Global:subnetName in $Global:SUBNET_NAMES) {
+	$Global:baseSubnetCommand = "$Global:GET_NSG_SUBNET_BASE $Global:subnetName"
+	Get_NSG_Subnet_Detailed
+	if ($Global:ecRc -eq $false ) {
+# Set-PSDebug -trace 1 -strict
+	    Create_NSG_Subnet
+	    if ($Global:ecRc -eq $true ) {
+		Get_NSG_Subnet_Detailed
+	    }
+	    else {
+		Write-ColorOutput "Yellow" ">> TESTING: `$Global:currentNSG = Invoke-Expression $thisCommand"
+	    }
+# Set-PSDebug -trace 0 -strict;Exit 1
+	}
+    }
+
+# Set-PSDebug -trace 0 -strict;Exit 1
+}
+#
+################################################################################
+
 
 
 ################################################################################
@@ -280,41 +386,16 @@ if ($Global:PERFORM_CREATE_RULE) { Set-PSDebug -trace 0 -strict;Exit 1 }
     Setup_NSG
 
     if ($Global:ecRc -eq $true) {
-	Setup_NSG_Rules $Global:currentNSG
+	Setup_NSG_Rules
     }
+
+    if ($Global:ecRc -eq $true) {
+	Setup_NSG_Subnets
+    }
+
 #
 ################################################################################
 
 
-Set-PSDebug -trace 0 -strict;Exit 1
-
-# Get info for the NSG assigned to the Web subnet
-<#
-$Global:currentNSG = Get-AzureNetworkSecurityGroup -Name $NSG_NAME
-Get-AzureNetworkSecurityGroup -Name $NSG_NAME -Detailed
-Get-AzureNetworkSecurityGroup -Name $NSG_NAME -Detailed > ATL_NSG.txt
-Get-AzureNetworkSecurityGroupForSubnet -VirtualNetworkName $VNET_NAME -SubnetName $subnetName -Detailed
-#>
-
-# Assign the NSG to all subnets
-<#
-$Global:currentNSG | Set-AzureNetworkSecurityGroupToSubnet -VirtualNetworkName $VNET_NAME -SubnetName $subnetName
-#>
-
-# Remove the NSG assigned to the Web subnet
-<#
-Remove-AzureNetworkSecurityGroupFromSubnet -VirtualNetworkName $VNET_NAME -SubnetName $subnetName -Name $NSG_NAME
-Remove-AzureNetworkSecurityGroup -Name $NSG_NAME
-#>
-
-# Define the NSG rules for all subnets
-$Global:currentNSG | Set-AzureNetworkSecurityRule -Name 'Internet_Inbound_Allow_1' -Type Inbound -Priority 100 -Action Allow -SourceAddressPrefix '83.102.45.48/28' -SourcePortRange '*' -DestinationAddressPrefix '*' -DestinationPortRange '*' -Protocol 'TCP'
-$Global:currentNSG | Set-AzureNetworkSecurityRule -Name 'Internet_Inbound_Allow_2' -Type Inbound -Priority 101 -Action Allow -SourceAddressPrefix '204.15.116.0/22' -SourcePortRange '*' -DestinationAddressPrefix '*' -DestinationPortRange '*' -Protocol 'TCP'
-$Global:currentNSG | Set-AzureNetworkSecurityRule -Name 'Internet_Inbound_Allow_3' -Type Inbound -Priority 102 -Action Allow -SourceAddressPrefix '63.73.199.0/24' -SourcePortRange '*' -DestinationAddressPrefix '*' -DestinationPortRange '*' -Protocol 'TCP'
-
-# Assign the NSG to all subnets
-$Global:currentNSG | Set-AzureNetworkSecurityGroupToSubnet -VirtualNetworkName $VNET_NAME -SubnetName WBA-ATL-WEB
-$Global:currentNSG | Set-AzureNetworkSecurityGroupToSubnet -VirtualNetworkName $VNET_NAME -SubnetName WBA-ATL-APP
-$Global:currentNSG | Set-AzureNetworkSecurityGroupToSubnet -VirtualNetworkName $VNET_NAME -SubnetName WBA-ATL-DB
-
-Exit 1
+Set-PSDebug -trace 0 -strict
+Exit 0
